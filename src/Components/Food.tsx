78e58c15a6f4ace584b60b8a8b4a69e6337e35ac
Ramses Godoy <74273214+Ramsescce@users.noreply.github.com>
2021-02-02 14:09:34 -0300

    Update Food.tsx

--- src/Components/Food.tsx
+++ src/Components/Food.tsx
@@ -1,55 +1,74 @@
-import React, {useEffect, useState} from 'react';
-import {foodMenu} from '../types/FoodType'
-import axios from 'axios'
-import {menuRefeicao} from '../types/Refeicoes'
+import React, { useEffect, useState } from "react";
+import { foodMenu } from "../types/FoodType";
+import axios from "axios";
+import { menuRefeicao } from "../types/Refeicoes";
 
 const Foods = () => {
-  const [foodCategoria, setCategoria] = useState<foodMenu[]>([])
-  const [buscaFood, setbusca] = useState<string>()
-  const [refeicoesDigi,setRefeicoes] =useState<menuRefeicao[]>([])
+  const [foodCategoria, setCategoria] = useState<foodMenu[]>([]);
+  const [buscaFood, setbusca] = useState<string>();
+  const [refeicoesDigi, setRefeicoes] = useState<menuRefeicao[]>([]);
+  const [search, setSearch] = useState<string>();
+  useEffect(() => {
+    axios
+      .get("https://www.themealdb.com/api/json/v1/1/categories.php")
+      .then((resposta) => setCategoria(resposta.data.categories));
+  }, []);
+  useEffect(() => {
+    if (buscaFood !== null)
+      axios
+        .get(
+          `https://www.themealdb.com/api/json/v1/1/filter.php?c=${buscaFood}`
+        )
+        .then((resposta) => setRefeicoes(resposta.data.meals));
+  }, [buscaFood]);
 
   useEffect(() => {
-    axios.get('https://www.themealdb.com/api/json/v1/1/categories.php')
-        .then(resposta => setCategoria(resposta.data.categories))
-  }, [])
-  useEffect(() => {
-    
-    axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${buscaFood}`)
-        .then(resposta => setbusca(resposta.data.meals))
-  }, [buscaFood])
-  
-  useEffect(() => {
-    axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${buscaFood}`)
-        .then(resposta => setRefeicoes(resposta.data.meals))
-  }, [buscaFood])
- 
-  
-    return (
+    if (search !== null)
+      axios
+        .get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
+        .then((resposta) => setRefeicoes(resposta.data.meals));
+  }, [search]);
 
+  return (
     <div className="food-beer-list food-shop">
-      <h1>Tipos de pratos</h1>
+      <h1>
+        <strong>AIFOME</strong>
+      </h1>
       <p>
         Selecione uma categoria ou digite a comida (em inglês):
-        <input type="text" placeholder="Digite a comida..." onChange={(event) => setbusca(event?.target.value)}/>
+        <input
+          type="text"
+          placeholder="Digite a comida..."
+          onChange={(event) => setSearch(event.target.value)}
+        />
       </p>
 
       <ul>
-      {
-            foodCategoria.map((item:foodMenu) => (
-              <div key={item.idCategory} className="catalog">
-              <img src={item.strCategoryThumb} alt={item.strCategory}/>
-              <li onClick={() => setbusca(item.strCategory)}>{item.strCategory}</li>
-              </div>
-            ))
-          }
+        {foodCategoria !== null &&
+          foodCategoria.map((item: foodMenu) => (
+            <li
+              key={item.idCategory}
+              onClick={() => setbusca(item.strCategory)}
+            >
+              {item.strCategory}
+            </li>
+          ))}
       </ul>
-      <h2>Tipo selecionado: <strong>{buscaFood}</strong></h2>
-      
-          <div className="food-container">
-        
-          </div>
-        </div>
-      );
-    }
+      <h2>
+        Tipo selecionado: <strong>{buscaFood}</strong>
+      </h2>
 
-    export default Foods;
+      <div className="food-container">
+        {refeicoesDigi !== null &&
+          refeicoesDigi.map((item: menuRefeicao) => (
+            <div className="food-item" key={item.idMeal}>
+              <img src={item.strMealThumb} />
+              <p>{item.strMeal}</p>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Foods;
